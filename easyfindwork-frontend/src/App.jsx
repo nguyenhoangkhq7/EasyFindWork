71afb8724ba8406a82656338030a5d0c08f33eeb
ToHoangThanh042004 <Thanhsgk123@gmail.com>
2025-05-04 21:12:08 +0700

    App Hien Thi Trng Chu Va Tim Kiem

--- easyfindwork-frontend/src/App.jsx
+++ easyfindwork-frontend/src/App.jsx
@@ -1,24 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Search from "./pages/Search";
-import JobDetail from "./pages/JobDetail";
-import Profile from "./pages/Profile";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+// import Home from "./pages/Home";
+// import Search from "./pages/Search";
+// import JobDetail from "./pages/JobDetail";
+// import Profile from "./pages/Profile";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+import JobPortal from "./components/JobPortal";
+import JobSearch from "./components/JobSearch";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/job/:id" element={<JobDetail />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+      <Route path="/" element={<JobPortal />} />
+        <Route path="/job-search" element={<JobSearch />} />
+
+    {/* //     <Route path="/search" element={<Search />} />
+    //     <Route path="/job/:id" element={<JobDetail />} />
+    //     <Route path="/profile" element={<Profile />} />
+    //     <Route path="/login" element={<Login />} />
+    //     <Route path="/register" element={<Register />} /> */}
       </Routes>
     </Router>
   );
+
 }
 
 export default App;
